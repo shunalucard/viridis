cf0685eb9c5dc490f46f3db8f30a722ce77c6227
shunalucard <dudiaguiar@gmail.com>
2024-06-06 22:51:32 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div></div>
+      <Text>Começo da pagina</Text>
+      <h1>PENIS</h1>
+      <h2>PENIS</h2>
+      <h3>PENIS</h3>
+      <h4>PENIS</h4>
+      <h5>PENIS</h5>
+      <h6>PENIS</h6>
+    </View>
+  );
+}
